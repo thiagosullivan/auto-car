145d571eb385dce027feb36bedcb7f785f162470
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-09-01 07:42:34 -0300

    feat: add brands carousel

---
+++ src/app/components/brands.tsx
@@ -0,0 +1,51 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import { brandsList } from "../../data";
+import Image from "next/image";
+
+const BrandsComponent = () => {
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-full max-w-11/12 mx-auto"
+    >
+      <CarouselContent>
+        {brandsList.map((brand) => (
+          <CarouselItem
+            key={brand.href}
+            className="basis-1/4 sm:basis-1/6 md:basis-1/7 lg:basis-1/10 xl:basis-1/12"
+          >
+            <div className="flex flex-col items-center gap-y-2">
+              <Card className="p-0 rounded-full">
+                <CardContent className="p-0 w-20 h-20 flex items-center justify-center">
+                  <Image
+                    src={brand.imageUrl}
+                    width={55}
+                    height={55}
+                    alt={brand.name}
+                  />
+                </CardContent>
+              </Card>
+              <p className="text-xs text-center font-semibold">{brand.name}</p>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <div className="max-md:hidden">
+        <CarouselPrevious />
+        <CarouselNext />
+      </div>
+    </Carousel>
+  );
+};
+
+export default BrandsComponent;
---
+++ src/data.js
@@ -0,0 +1,207 @@
+export const brandsList = [
+  {
+    name: "Aston Martin",
+    imageUrl: "/brands/aston-martin-logo.png",
+    href: "/aston-martin",
+  },
+  {
+    name: "Audi",
+    imageUrl: "/brands/audi-logo.png",
+    href: "/audi",
+  },
+  {
+    name: "Bentley",
+    imageUrl: "/brands/bentley-logo.png",
+    href: "/bentley",
+  },
+  {
+    name: "BMW",
+    imageUrl: "/brands/bmw-logo.png",
+    href: "/bmw",
+  },
+  {
+    name: "BYD",
+    imageUrl: "/brands/byd-logo.png",
+    href: "/byd",
+  },
+  {
+    name: "Chery",
+    imageUrl: "/brands/chery-logo.png",
+    href: "/chery",
+  },
+  {
+    name: "Chevrolet",
+    imageUrl: "/brands/chevrolet-logo.png",
+    href: "/chevrolet",
+  },
+  {
+    name: "Chrysler",
+    imageUrl: "/brands/chrysler-logo.png",
+    href: "/chrysler",
+  },
+  {
+    name: "Citroen",
+    imageUrl: "/brands/citroen-logo.png",
+    href: "/citroen",
+  },
+  {
+    name: "Dodge",
+    imageUrl: "/brands/dodge-logo.png",
+    href: "/dodge",
+  },
+  {
+    name: "Ferrari",
+    imageUrl: "/brands/ferrari-logo.png",
+    href: "/ferrari",
+  },
+  {
+    name: "Fiat",
+    imageUrl: "/brands/fiat-logo.png",
+    href: "/fiat",
+  },
+  {
+    name: "Ford",
+    imageUrl: "/brands/ford-logo.png",
+    href: "/ford",
+  },
+  {
+    name: "GAC",
+    imageUrl: "/brands/gac-logo.png",
+    href: "/gac",
+  },
+  {
+    name: "GWM",
+    imageUrl: "/brands/gwm-logo.png",
+    href: "/gwm",
+  },
+  {
+    name: "Honda",
+    imageUrl: "/brands/honda-logo.png",
+    href: "/honda",
+  },
+  {
+    name: "Hyundai",
+    imageUrl: "/brands/hyundai-logo.png",
+    href: "/hyundai",
+  },
+  {
+    name: "Jaguar",
+    imageUrl: "/brands/jaguar-logo.png",
+    href: "/jaguar",
+  },
+  {
+    name: "Jeep",
+    imageUrl: "/brands/jeep-logo.png",
+    href: "/jeep",
+  },
+  {
+    name: "Kia",
+    imageUrl: "/brands/kia-logo.png",
+    href: "/kia",
+  },
+  {
+    name: "Lamborghini",
+    imageUrl: "/brands/lamborghini-logo.png",
+    href: "/lamborghini",
+  },
+  {
+    name: "Land-rover",
+    imageUrl: "/brands/land-rover-logo.png",
+    href: "/land-rover",
+  },
+  {
+    name: "Lexus",
+    imageUrl: "/brands/lexus-logo.png",
+    href: "/lexus",
+  },
+  {
+    name: "Maserati",
+    imageUrl: "/brands/maserati-logo.png",
+    href: "/maserati",
+  },
+  {
+    name: "Mclaren",
+    imageUrl: "/brands/mclaren-logo.png",
+    href: "/mclaren",
+  },
+  {
+    name: "Mercedes",
+    imageUrl: "/brands/mercedes-logo.png",
+    href: "/mercedes",
+  },
+  {
+    name: "Mini",
+    imageUrl: "/brands/mini-logo.png",
+    href: "/mini",
+  },
+  {
+    name: "Mitsubishi",
+    imageUrl: "/brands/mitsubishi-logo.png",
+    href: "/mitsubishi",
+  },
+  {
+    name: "Nissan",
+    imageUrl: "/brands/nissan-logo.png",
+    href: "/nissan",
+  },
+  {
+    name: "Omoda-Jaecoo",
+    imageUrl: "/brands/omoda-jaecoo-logo.png",
+    href: "/omoda-jaecoo",
+  },
+  {
+    name: "Peugeot",
+    imageUrl: "/brands/peugeot-logo.png",
+    href: "/peugeot",
+  },
+  {
+    name: "Porsche",
+    imageUrl: "/brands/porsche-logo.png",
+    href: "/porsche",
+  },
+  {
+    name: "Ram",
+    imageUrl: "/brands/ram-logo.png",
+    href: "/ram",
+  },
+  {
+    name: "Renault",
+    imageUrl: "/brands/renault-logo.png",
+    href: "/renault",
+  },
+  {
+    name: "Smart",
+    imageUrl: "/brands/smart-logo.png",
+    href: "/smart",
+  },
+  {
+    name: "Subaru",
+    imageUrl: "/brands/subaru-logo.png",
+    href: "/subaru",
+  },
+  {
+    name: "Tesla",
+    imageUrl: "/brands/tesla-logo.png",
+    href: "/tesla",
+  },
+  {
+    name: "Toyota",
+    imageUrl: "/brands/toyota-logo.png",
+    href: "/toyota",
+  },
+  {
+    name: "Troller",
+    imageUrl: "/brands/troller-logo.png",
+    href: "/troller",
+  },
+  {
+    name: "Volkswagen",
+    imageUrl: "/brands/volkswagen-logo.png",
+    href: "/volkswagen",
+  },
+  {
+    name: "Volvo",
+    imageUrl: "/brands/volvo-logo.png",
+    href: "/volvo",
+  },
+];
