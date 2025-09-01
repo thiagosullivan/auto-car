5c5779248e51815c6e616b83c4c9e62b620faa17
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-09-01 06:17:42 -0300

    feat: add header component

---
+++ src/components/commons/header.tsx
@@ -0,0 +1,67 @@
+import { Heart, Search } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const navLinks = [
+  {
+    href: "/",
+    name: "Home",
+  },
+  {
+    href: "/estoque",
+    name: "Estoque",
+  },
+  {
+    href: "/venda",
+    name: "Venda Seu Carro",
+  },
+  {
+    href: "/financiamento",
+    name: "Financiamento",
+  },
+  {
+    href: "/sobre-nos",
+    name: "Empresa",
+  },
+  {
+    href: "/favorito",
+    name: "Favorito",
+  },
+  {
+    href: "/contato",
+    name: "Contato",
+  },
+];
+
+const Header = () => {
+  return (
+    <header className="shadow-md bg-white py-4">
+      <div className="mx-auto max-w-7xl w-full px-4 flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src="/auto-car-short-logo.png"
+            width={84}
+            height={71}
+            alt="Auto Car"
+          />
+        </Link>
+        <nav>
+          <ul className="flex gap-4">
+            {navLinks.map((link) => (
+              <Link href={link.href} key={link.name} className="text-gray-one">
+                {link.name}
+              </Link>
+            ))}
+          </ul>
+        </nav>
+        <div className="flex items-center gap-3 text-gray-one">
+          <Search />
+          <Heart />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
