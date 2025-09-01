ee1e42343ac51111082fb58529067739d83fa60e
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-09-01 06:31:06 -0300

    feat: add hero banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <h1>home</h1>;
+  return (
+    <main className="mt-6">
+      {/* Hero */}
+      <section className="w-full max-w-7xl mx-auto h-[450px] relative">
+        <Image
+          src="https://i.ibb.co/CKzPmz0B/auto-car-banner.jpg"
+          alt="banner"
+          sizes="100vw"
+          fill
+        />
+      </section>
+    </main>
+  );
 }
