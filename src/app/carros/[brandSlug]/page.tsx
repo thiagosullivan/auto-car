e008406ea577b3a3392d6c69f11162c60f6d0191
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-09-03 08:06:40 -0300

    feat: getting cars data from db to brand page

--- src/app/carros/[brandSlug]/page.tsx
+++ src/app/carros/[brandSlug]/page.tsx
@@ -1,4 +1,9 @@
+import CarCards from "@/components/commons/carCards";
 import FinancingAlert from "@/components/commons/financingAlert";
+import { db } from "@/db";
+import { brandTable, carTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { notFound } from "next/navigation";
 import React from "react";
 
 interface CategoryPageProps {
@@ -7,14 +12,40 @@ interface CategoryPageProps {
 
 const BrandPage = async ({ params }: CategoryPageProps) => {
   const { brandSlug } = await params;
+  const brand = await db.query.brandTable.findFirst({
+    where: eq(brandTable.slug, brandSlug),
+    with: {
+      cars: true,
+    },
+  });
 
-  console.log(brandSlug, "SLUG");
+  if (!brand) {
+    return notFound();
+  }
+
+  const carsList = await db.query.carTable.findMany({
+    where: eq(carTable.brandId, brand.id),
+    with: {
+      brand: true,
+    },
+  });
+
+  console.log(carsList, "cars slug");
+
   return (
     <div className="w-full max-w-7xl mx-auto my-10 text-gray-one px-4">
-      <h1 className="text-3xl font-bold mb-8 capitalize">{brandSlug}</h1>
+      <h1 className="text-3xl font-bold mb-8 capitalize">{brand.name}</h1>
       <div className="mb-8">
         <div className="my-10">
-          <p>Nenhum veículo dessa marca foi encontrado :(</p>
+          {carsList.length > 0 ? (
+            <>
+              {carsList.map((car) => (
+                <CarCards key={car.id} cars={carsList} />
+              ))}
+            </>
+          ) : (
+            <p>Nenhum veículo dessa marca foi encontrado :(</p>
+          )}
         </div>
       </div>
       <FinancingAlert />
